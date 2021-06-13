c4cbd66560baef78e43a98381413c6ad8ba8abf9
Elias Goñez <eliasg.videla@gmail.com>
2021-06-13 02:42:37 -0300

    agrego todos los inserts a go

--- cli.go
+++ cli.go
@@ -47,6 +47,10 @@ func ejecutar_opcion(opcion_elegida int) {
 	} else if opcion_elegida == 2 {
 
 		crear_tablas()
+		
+	} else if opcion_elegida == 3 {
+		
+		llenar_tablas()	
 
 	} else if opcion_elegida == 4 {
 
@@ -78,11 +82,11 @@ func crear_bdd() {
 	defer db.Close()
 
 	//creamos nuestra base de datos
-	_, err = db.Exec(`drop database if exists tarjetasdecredito`)
+	_, err = db.Exec(`drop database if exists basedatos`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = db.Exec(`create database tarjetasdecredito`)
+	_, err = db.Exec(`create database basedatos`)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -110,18 +114,82 @@ func conectar_con_bdd() *sql.DB {
 func crear_tablas() {
 
 	db := conectar_con_bdd() // conectamos a nuestra base de datos
-	defer db.Close()         // alterminar de ejecutar todo lo que esta en el cuerpo de esta funcion se cierra la bdd hay que hacer esto
+	//defer db.Close()         // alterminar de ejecutar todo lo que esta en el cuerpo de esta funcion se cierra la bdd hay que hacer esto
 	// en cada funcion que creemos.
-	_, err := db.Exec(`create table cliente(nroCliente int, nombre text, apellido text, domicilio text, telefono char(12));`)
+	
+	_, err := db.Exec(`create table cliente(nrocliente int, nombre text, apellido text, domicilio text, telefono char(12));
+
+create table tarjeta(nrotarjeta char(16), nrocliente int, validadesde char(6), validahasta char(6), codseguridad char(4), limitecompra decimal(8,2), estado char(10));
+
+create table comercio(nrocomercio int, nombre text, domicilio text, codigopostal char(8), telefono char(12));
+
+create table compra(nrooperacion serial, nrotarjeta char(16), nrocomercio int, fecha timestamp, monto decimal(7,2), pagado boolean);
+
+create table rechazo(nrorechazo serial, nrotarjeta char(16), nrocomercio int, fecha timestamp, monto decimal(7,2), motivo text);
+
+create table cierre(año int, mes int, terminacion int, fechainicio date, fechacierre date, fechavto date);
+
+create table cabecera(nroresumen serial, nombre text, apellido text, domicilio text, nrotarjeta char(16), desde date, hasta date, vence date, total decimal(8,2));
+
+create table detalle(nroresumen serial, nrolinea int, fecha date, nombrecomercio text, monto decimal(7,2));
+
+create table alerta(nroalerta serial, nrotarjeta char(16),fecha timestamp, nrorechazo int, codalerta int, descripcion text);
+
+create table consumo(nrotarjeta char(16), codseguridad char(4), nrocomercio int, monto decimal(7,2));`)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	crear_pk_fk()
 
 	//hace falta aqui una funcion que cree las pk y fk
 
 	fmt.Printf("\n### Tablas creadas ###\n")
 }
+
+
+//Funcion que crea todas las pk's y fk's--------------------------------------------------------------------
+
+func crear_pk_fk(){
+	
+		db := conectar_con_bdd()
+		
+		
+		_,err := db.Exec(`alter table cliente  add constraint cliente_pk  primary key (nrocliente);
+alter table tarjeta  add constraint tarjeta_pk  primary key (nrotarjeta);
+alter table comercio add constraint comercio_pk primary key (nrocomercio);
+alter table compra   add constraint compra_pk   primary key (nrooperacion);
+alter table rechazo  add constraint rechazo_pk  primary key (nrorechazo);
+alter table cierre   add constraint cierre_pk   primary key (año, mes, terminacion);
+alter table cabecera add constraint cabecera_pk primary key (nroresumen);
+alter table detalle  add constraint detalle_pk  primary key (nroresumen, nrolinea);
+alter table alerta   add constraint alerta_pk   primary key (nroalerta);
+
+alter table tarjeta  add constraint tarjeta_nrocliente_fk  foreign key (nrocliente)  references cliente(nrocliente);
+
+alter table compra   add constraint compra_nrotarjeta_fk   foreign key (nrotarjeta)  references tarjeta(nrotarjeta);
+alter table compra   add constraint compra_nrocomercio_fk  foreign key (nrocomercio) references comercio(nrocomercio);
+
+alter table rechazo  add constraint rechazo_nrotarjeta_fk  foreign key (nrotarjeta)  references tarjeta(nrotarjeta);
+alter table rechazo  add constraint rechazo_nrocomercio_fk foreign key (nrocomercio) references comercio(nrocomercio);
+
+alter table cabecera add constraint cabecera_nrotarjeta_fk foreign key (nrotarjeta)  references tarjeta(nrotarjeta);
+
+alter table detalle  add constraint detalle_nroresumen_fk  foreign key (nroresumen)  references cabecera(nroresumen);
+
+alter table alerta   add constraint alerta_nrotarjeta_fk   foreign key (nrotarjeta)  references tarjeta(nrotarjeta);
+alter table alerta   add constraint alerta_nrorechazo_fk   foreign key (nrorechazo)  references rechazo(nrorechazo);
+
+alter table consumo  add constraint consumo_nrotarjeta_fk  foreign key (nrotarjeta)  references tarjeta(nrotarjeta);
+alter table consumo  add constraint consumo_nrocomercio_fk foreign key (nrocomercio) references comercio(nrocomercio);`)
+		
+		if err != nil{
+				log.Fatal(err)
+		}
+		
+}
+
 
 //Inserts----------------------------------------------------------------------------------------------------
 func llenar_tablas() {
@@ -129,7 +197,85 @@ func llenar_tablas() {
 	db := conectar_con_bdd()
 	defer db.Close()
 
-	_, err := db.Exec(`insert into cliente values(1,'Daniela Anabel','Oviedo','San Martin 3814','541130569988')`)
+	_, err := db.Exec(`insert into cliente values(1,'Daniela Anabel','Oviedo','San Martin 3814','541130569988');
+insert into cliente values(2,'Fernando','Ferreyra','Benito Lynch 2206','541156441305');
+insert into cliente values(3, 'Elias','Goñez', 'Valparaiso 2050','541128898392');
+insert into cliente values(4,'Romina','Segretin','Uruguay 790','541154085062');
+insert into cliente values(5,'Fabian','García Gómez','Av. Alem 368','541140495127');
+insert into cliente values(6,'Matheo Samuel','García','Av. Callao 1311','541146155914');
+insert into cliente values(7,'Sabrina Rosalia','Ramirez','Av. Sáenz 945','541130360237');
+insert into cliente values(8,'Sara Valeria','Hernández','Av. Cabildo 2523','541128447247');
+insert into cliente values(9,'Alicia Grisel','Gómez','Cura Brochero 1053','541161206314');
+insert into cliente values(10,'Joana Elizabeth','Villarreal','Palpa 1020','541164294818');
+insert into cliente values(11,'Ignacio Ariel','Perez','Paso de los patos 2508','541189768847');
+insert into cliente values(12,'Lucia Daniela','Benitez','Av. Rivadavia 2199','541124361554');
+insert into cliente values(13,'Maximiliano Ezequiel','Fernandez','Obrien 2460','541167353600');
+insert into cliente values(14,'Cristian Elias','Oviedo','Yatasto 1749','541126858087');
+insert into cliente values(15,'Carolina Noelia','Diaz','Falucho 853','541151955038');
+insert into cliente values(16,'Agustina','Lopez','Ricardo Rojas 1183','541141612153');
+insert into cliente values(17,'Luciano Damian','Mansilla','Av. Eva Duarte de Perón 904','541136471202');
+insert into cliente values(18,'Hernan Daniel','Rondelli','Nazca 1065','541127146757');
+insert into cliente values(19,'Leandro David','Gimenez','Juan Maria Gutiérrez 1150','541125405212');
+insert into cliente values(20,'Rodrigo Ezquiel','Palacios','Pablo Areguati 299','541124511771');
+
+
+
+--tarjetas
+insert into tarjeta values('4286283215095190', 1, '201709', '202208', '114', 45000.00, 'vigente');
+insert into tarjeta values('4532449515464319', 2, '202001', '202412', '881', 30000.00, 'vigente');
+insert into tarjeta values('4716905901199213', 3, '202108', '202607', '311', 150000.00, 'vigente');
+insert into tarjeta values('4539760286740064', 4, '202204', '202703', '553', 35000.00, 'vigente');
+insert into tarjeta values('4916197097056062', 5, '202010', '202509', '103', 45000.00, 'anulada');
+insert into tarjeta values('4532157860627139', 6, '202004', '202503', '802', 42000.00, 'anulada');
+insert into tarjeta values('4449942525596585', 7, '202010', '202509', '552', 120000.00, 'vigente');
+insert into tarjeta values('4929028998516745', 8, '201610', '202109', '412', 110000.00, 'suspendida');
+insert into tarjeta values('4916558526474988', 9, '201604', '202103', '633', 65000.00, 'anulada');--vencida
+insert into tarjeta values('4456844734152285', 10, '201707', '202206', '853', 35000.00, 'anulada');
+insert into tarjeta values('5305073210930499', 11, '201707', '202206', '271', 140000.00, 'vigente');
+insert into tarjeta values('5115874922952014', 12, '202008', '202507', '647', 70000.00, 'suspendida');
+insert into tarjeta values('5433516727758253', 13, '201802', '202301', '345', 150000.00, 'vigente');
+insert into tarjeta values('5200557813577356', 14, '201707', '202206', '112', 120000.00, 'anulada');
+insert into tarjeta values('5425807573408337', 15, '201712', '202211', '879', 43000.00, 'vigente');
+insert into tarjeta values('5255982663365344', 16, '201906', '202405', '768', 120000.00, 'suspendida');
+insert into tarjeta values('5535292533476491', 17, '201805', '202304', '876', 170000.00, 'vigente');
+insert into tarjeta values('5425758312840399', 18, '202005', '202504', '881', 80000.00, 'vigente');
+insert into tarjeta values('340869936801114', 17, '201907', '202406', '675', 90000.00, 'vigente'); 
+insert into tarjeta values('342888106007110', 18, '202103', '202602', '127', 120000.00, 'vigente');
+insert into tarjeta values('343263611209214', 19, '201909', '202408', '901', 200000.00, 'anulada');
+insert into tarjeta values('377829618815820', 20, '201804', '202303', '320', 90000.00, 'suspendida');
+
+
+--comercios
+insert into comercio values(1, 'Coto', 'Belgrano 960', 'B1619JHU','034844458867');--comercios con el mismo CP
+insert into comercio values(2, 'Sodimac','Constituyentes 1370','B1619JHU','112658423658');--comercios con el mismo CP
+insert into comercio values(3, 'Buen Gusto', 'Av. Libertador 3072', 'C1245YTD','541126598965');
+insert into comercio values(4, 'Cafeteria Victor', 'Juan Gutierrez 1150', 'B1613GAE', '541178451245');
+insert into comercio values(5, 'Libreria Alondra', 'Mateo Churich 130', 'B1619JGB', '541125584518');
+insert into comercio values(6, 'Carrefour', 'Los Andes 458', 'B1608OKL', '541126154879');
+insert into comercio values(7, 'El Boulevard', 'General Peron 377', 'B1610HGU', '541128964712');
+insert into comercio values(8, 'Rapanui', 'Juan Domingo Peron 1974', 'C1456NSM', '541126597841');
+insert into comercio values(9, 'Ñoquis Artesanales', 'Balcarce 50', 'C1064KCF', '541143443600');
+insert into comercio values(10, 'McDonalds', 'Hipolito Yrigoyen 267', 'B1610LPN','541126455468');
+insert into comercio values(11, 'Starbucks', '25 de Mayo 2254', 'B1609KGJ','541148897234');
+insert into comercio values(12, 'Parrilla El Chorizon', 'Blandengues 483', 'B1611HGE', '541136223879');
+insert into comercio values(13, 'Optica Casimiro', 'Juan B Justo 2020', 'C1032BND', '541121145469');
+insert into comercio values(14, 'Ferreteria El Cosito', 'Peru 1654', 'B1663ERF', '541136458159');
+insert into comercio values(15, 'Farmacia Favaloro', 'Remedios de Escalada 392', 'B1619HJU', '541125698741');
+insert into comercio values(16, 'Servicio Tecnico LG', 'Marie Curie 506', 'B1600KIB', '541125896734');
+insert into comercio values(17, 'Loteria de la provincia', 'Cayetano Bourdet 2390', 'B1619PER', '541123698564');
+insert into comercio values(18, 'Supermercado Puma', 'Cordoba 212', 'B1610GBF', '541128955864');
+insert into comercio values(19, 'Aberturas Pepe', '9 de Julio 3004', 'C1040JUG', '541126897468');
+insert into comercio values(20, 'Cinemark', 'Constituyentes 2078', 'B1620MVU', '541128969864');
+
+
+--consumos
+insert into consumo values('4716905901199213', '311', 10, 750.00);
+insert into consumo values('5305073210930499', '271', 6, 1500.00);
+insert into consumo values('5535292533476491', '876', 1, 3000.00);
+insert into consumo values('4916197097056062', '103', 11, 500.00);
+insert into consumo values('5425758312840399', '881', 15, 1000.00);
+insert into consumo values('4449942525596585', '552', 12, 2000.00);
+insert into consumo values('4286283215095190', '114', 14, 550.00);`)
 
 	if err != nil {
 		log.Fatal(err)
