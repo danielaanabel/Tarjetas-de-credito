0447ff248d73f4d9d0dce885a6fe4e3f4c003305
Daniela Oviedo <danielaanabel2011@gmail.com>
2021-06-14 09:30:20 -0300

    cambie la funcion resumen

--- BaseDatos2.sql
+++ BaseDatos2.sql
@@ -224,7 +224,7 @@ begin
 for i in i..n loop
     for j in j..m loop
         insert into cierre values(2021, j+1, i, fecha_inicio, fecha_cierre, fecha_vencimiento);
-        if (extract(year FROM fecha_vencimiento) = 2022) then
+        if (extract(year from fecha_vencimiento) = 2022) then
             fecha_inicio := fecha_inicio - cast('11 month' as interval);
             fecha_cierre := fecha_cierre - cast('11 month' as interval);
             fecha_vencimiento := fecha_vencimiento - cast('11 month' as interval);
@@ -352,7 +352,7 @@ for each row
 execute procedure func_alerta_compra();
 
 --función para generar el resumen de les clientes
-create or replace function generar_resumen(nro_cliente int, periodo char(6)) returns void as $$
+create or replace function generar_resumen(nro_cliente int, periodo_año int, periodo_mes int) returns void as $$
 declare
     dato_cliente record;
     tarjeta_cliente record;
@@ -360,16 +360,16 @@ declare
     total_a_pagar decimal(8,2);
     fila_compras record;
     contador_linea int := 1;
-    periodo_cast int :=(select cast((EXTRACT(MONTH FROM date(to_date(periodo, 'YYYYMM')))) as int));
+   
 begin
     select * into dato_cliente from cliente where nrocliente = nro_cliente;
    
     for tarjeta_cliente in select * from tarjeta where nrocliente = nro_cliente loop  --para cada tarjeta del cliente hacemos...
         
-        select * into dato_cierre from cierre where terminacion = (cast(substr(tarjeta_cliente.nrotarjeta, length(tarjeta_cliente.nrotarjeta)) as int)) 
-        and mes = periodo_cast;--obtener los datos de cierre para esa tarjeta de le cliente y para ese periodo  
+        select * into dato_cierre from cierre where terminacion = (cast(substr(tarjeta_cliente.nrotarjeta, length(tarjeta_cliente.nrotarjeta)) as int))--obtener la terminacion de esa tarjeta
+        and mes = periodo_mes;--obtener los datos de cierre para esa tarjeta de le cliente y para ese periodo  
 
-                total_a_pagar:= (select sum(monto) from compra c where nrotarjeta = tarjeta_cliente.nrotarjeta ); --sumamos el total de compras para esa tarjeta 
+                total_a_pagar:= (select sum(monto) from compra where nrotarjeta = tarjeta_cliente.nrotarjeta); --sumamos el total de compras para esa tarjeta 
 
                 insert into cabecera (nombre, apellido, domicilio, nrotarjeta, desde, hasta, vence, total)
                 values(dato_cliente.nombre, dato_cliente.apellido, dato_cliente.domicilio, tarjeta_cliente.nrotarjeta, 
@@ -420,7 +420,7 @@ select realizar_compras();
 
 select * from compra where nrotarjeta = '5425758312840399';
 
-select generar_resumen(18,'202106');
+select generar_resumen(18, 2021, 5);
 
 select * from cabecera;
 
